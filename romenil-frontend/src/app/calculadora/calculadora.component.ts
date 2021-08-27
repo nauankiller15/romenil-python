f31cbced52258398c82ba50db911f53cacba67c5
nauankiller15 <nauan.alcantara1@gmail.com>
2021-08-27 17:35:16 -0300

    reajuste visual na calculadora

--- romenil-frontend/src/app/calculadora/calculadora.component.ts
+++ romenil-frontend/src/app/calculadora/calculadora.component.ts
@@ -24,9 +24,10 @@ export class CalculadoraComponent implements OnInit {
       $('body').removeClass('noborder');
     });
     $('body').addClass('noborder');
-    // 
+    //
     $('#calculated').on('click', function () {
       $('#calculadoraForm').slideUp('100');
+      $('.cardBtn').slideDown(200);
     });
   }
 }
