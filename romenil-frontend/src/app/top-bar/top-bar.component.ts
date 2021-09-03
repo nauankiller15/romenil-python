bc748fe5d5e1240b7dff3cc277ff628a98906e71
nauankiller15 <nauan.alcantara1@gmail.com>
2021-09-03 18:08:50 -0300

    ajuste tela de login

--- romenil-frontend/src/app/top-bar/top-bar.component.ts
+++ romenil-frontend/src/app/top-bar/top-bar.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    window.onload = function () {
+      // MENU TOP BAR
+      const menu = document.querySelector("#mobile-menu");
+      const menuLinks = document.querySelector(".nav-menu");
+    
+      menu!.addEventListener("click", function () {
+        menu!.classList.toggle("is-active");
+        menuLinks!.classList.toggle("active");
+      });
+    };
+    
   }
-
 }
--- romenil-frontend/src/assets/static/JS/menu.anim.js
+++ romenil-frontend/src/assets/static/JS/menu.anim.js
@@ -1,13 +1,13 @@
-$(function() {
-    $(document).scroll(function() {
-        var $nav = $(".menu");
-        $nav.toggleClass('menu-back', $(this).scrollTop() > $nav.height());
-    });
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".menu");
+    $nav.toggleClass("menu-back", $(this).scrollTop() > $nav.height());
+  });
 });
 
 function MenuAtivo() {
-    $('.menuItems li a').on('click', function() {
-        $(this).addClass('active');
-        $(this).siblings().removeClass('active');
-    });
+  $(".menuItems li a").on("click", function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
 }
