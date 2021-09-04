4cf95cd38138fe5061d040a379908935b1978cfa
nauankiller15 <nauan.alcantara1@gmail.com>
2021-09-03 22:48:49 -0300

    menu dropdown acrescentado

--- romenil-frontend/src/app/top-bar/top-bar.component.ts
+++ romenil-frontend/src/app/top-bar/top-bar.component.ts
@@ -19,5 +19,29 @@ export class TopBarComponent implements OnInit {
       menu!.classList.toggle('is-active');
       menuLinks!.classList.toggle('active');
     });
+
+    // MENU DROP DOWN
+    const drop_btn = <HTMLElement>document.querySelector('.drop-btn');
+    const menu_wrapper = <HTMLElement>document.querySelector('.wrapper');
+    const menu_bar = <HTMLElement>document.querySelector('.menu-bar');
+    const setting_drop = <HTMLElement>document.querySelector('.setting-drop');
+    const setting_item = <HTMLElement>document.querySelector('.setting-item');
+    const back_setting_btn = <HTMLElement>(
+      document.querySelector('.back-setting-btn')
+    );
+
+    drop_btn!.addEventListener('click', function () {
+      menu_wrapper!.classList.toggle('show');
+    });
+    setting_item!.addEventListener('click', function () {
+      menu_bar!.style.marginLeft = '-300px';
+      setTimeout(() => {
+        setting_drop!.style.display = 'block';
+      }, 100);
+    });
+    back_setting_btn!.addEventListener('click', function () {
+      menu_bar!.style.marginLeft = '0px';
+      setting_drop!.style.display = 'none';
+    });
   }
 }
