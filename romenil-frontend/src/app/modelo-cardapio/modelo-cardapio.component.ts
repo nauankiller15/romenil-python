d114b46b26aa0d055e9d0d553a11ed038087fe0a
nauankiller15 <nauan.alcantara1@gmail.com>
2021-10-14 20:19:52 -0300

    ajustes

--- romenil-frontend/src/app/modelo-cardapio/modelo-cardapio.component.ts
+++ romenil-frontend/src/app/modelo-cardapio/modelo-cardapio.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-modelo-cardapio',
   templateUrl: './modelo-cardapio.component.html',
-  styleUrls: ['./modelo-cardapio.component.css']
+  styleUrls: ['./modelo-cardapio.component.css'],
 })
 export class ModeloCardapioComponent implements OnInit {
-
   @Input() cardapio: string = '';
+  eulaContent = '';
 
-  constructor() { }
-
+  constructor() {}
   ngOnInit(): void {
+    $(document).ready(function () {
+      $('.date').load('a.html');
+    });
   }
-
 }
