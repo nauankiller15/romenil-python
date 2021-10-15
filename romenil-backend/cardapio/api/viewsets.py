e9e20defd2af9d193a8a81d297181d278779752f
Daniel Chaves de Lima <danielchaves001@gmail.com>
2021-10-15 15:43:28 -0300

    seleção dos cardapios pronta

--- romenil-backend/cardapio/api/viewsets.py
+++ romenil-backend/cardapio/api/viewsets.py
@@ -2,15 +2,18 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ViewSet
 
+from formulario.models import Patologia
+
+from .cardapio import gerar_cardapio
+
 
 class CardapioViewSet(ViewSet):
     permission_classes = [IsAuthenticated]
 
-    def list(self, request):      
-        cardapio = 'PNPSNP'  
+    def list(self, request):  
+
+        usuario = request.user.usuario_set.last()
+        patologia = usuario.patologia_set.last()
+        cardapio = gerar_cardapio(patologia)
 
         return Response({'cardapio': cardapio})
-
-    def create(self, request, *args, **kwargs):
-        request.data['usuario'] = request.user.id
-        return super().create(request, *args, **kwargs)
