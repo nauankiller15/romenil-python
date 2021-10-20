1c7a0517ada0914b3ae48d68036ffe2987642e4f
nauankiller15 <nauan.alcantara1@gmail.com>
2021-10-20 00:21:06 -0300

    jquery mask e ajustes implementados

--- romenil-frontend/src/app/account/create-account/create-account.component.ts
+++ romenil-frontend/src/app/account/create-account/create-account.component.ts
@@ -42,6 +42,11 @@ export class CreateAccountComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    $(document).ready(() => {
+      $('.cep').mask('00000-000');
+      $('.celular').mask('(00) 00000-0000');
+      $('.cpf').mask('000.000.000-00', { reverse: false });
+    });
     // this.registerForm = this.formBuilder.group(
     //   {
     //     title: ['', Validators.required],
