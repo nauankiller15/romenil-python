a56fa6b50ab50ef634039d2d86a788390fab2228
Daniel Chaves <danielchaves001@gmail.com>
2021-10-21 02:49:56 -0300

    modificacao para formulario unico

--- romenil-backend/formulario/api/viewsets.py
+++ romenil-backend/formulario/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, ViewSet
@@ -12,8 +13,18 @@ class FormularioViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        request.data['usuario'] = request.user.usuario_set.last().id
-        return super().create(request, *args, **kwargs)
+        usuario = request.user.usuario_set.last()
+        formulario = usuario.formulario_set.last()
+        if formulario:
+            serializer = self.get_serializer(formulario, data=request.data)
+        else:
+            request.data['usuario'] = usuario.id
+            serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def list(self, request, *args, **kwargs):
         usuario = request.user.usuario_set.last()
