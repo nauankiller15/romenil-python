6de402ad39ac8aa445898eb033aab46645bb1ebc
Daniel Chaves <danielchaves001@gmail.com>
2021-10-21 01:57:55 -0300

    remoção de prints

--- romenil-backend/cardapio/api/viewsets.py
+++ romenil-backend/cardapio/api/viewsets.py
@@ -22,8 +22,6 @@ class CardapioViewSet(ViewSet):
         pronto = False
 
         if Formulario:
-            print(datetime.now(timezone.utc), formulario.modificado_em, datetime.now(timezone.utc) - timedelta(hours=2))
-            print(formulario.modificado_em < datetime.now(timezone.utc) - timedelta(hours=2))
             if formulario.modificado_em < datetime.now(timezone.utc) - timedelta(hours=2):
                 cardapios = gerar_cardapio(formulario)
                 dados = CardapioSerializer(cardapios, many=True).data
