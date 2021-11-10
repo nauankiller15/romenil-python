a7372f69f0a6e2d066d8eb121e4346c68b90ed0c
nauankiller15 <nauan.alcantara1@gmail.com>
2021-11-09 21:22:33 -0300

    ajustes visuais

--- romenil-frontend/src/app/app.component.ts
+++ romenil-frontend/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterEvent } from '@angular/router';
+import {
+  RouteConfigLoadEnd,
+  RouteConfigLoadStart,
+  Router,
+  RouterEvent,
+} from '@angular/router';
 
 declare var $: any;
 @Component({
@@ -12,28 +17,12 @@ export class AppComponent implements OnInit {
   title(title: any) {
     throw new Error('Method not implemented.');
   }
-  loading: boolean;
 
-  constructor(private router: Router) {
-
-    this.loading = false;
-
-    // router.events.subscribe((event: RouterEvent) => {
-    //   if (event instanceof RouteConfigLoadStart) {
-    //     this.loading = true;        
-    //   }
-    //   else if (event instanceof RouteConfigLoadEnd) {
-    //     this.loading = false;
-    //   }
-    // }
-
-  }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     $('#entrarprog').on('click', function () {
-
       $('.dashFundo').fadeIn(250);
-
     });
   }
 }
