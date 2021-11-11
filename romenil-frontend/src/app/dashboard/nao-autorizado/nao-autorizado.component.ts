b1b74d66ee0fd1ef5be8c08aa43830088d271982
nauankiller15 <nauan.alcantara1@gmail.com>
2021-11-10 23:17:12 -0300

    página de nao-autorizado ajustado

--- romenil-frontend/src/app/dashboard/nao-autorizado/nao-autorizado.component.ts
+++ romenil-frontend/src/app/dashboard/nao-autorizado/nao-autorizado.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-nao-autorizado',
   templateUrl: './nao-autorizado.component.html',
@@ -10,6 +12,14 @@ export class NaoAutorizadoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function () {
+      $('.loader-wrapper').fadeIn(250).delay(1000).fadeOut(250);
+    });
+    $('#fecharBtAguarde').on('click', function () {
+      $('#naoautorizado').fadeOut('100');
+      $('body').removeClass('noborder');
+    });
+    $('body').addClass('noborder');
   }
 
 }
