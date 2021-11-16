374ccc249b4acad5a7eb691e1da378b64e935059
Daniel Chaves <danielchaves001@gmail.com>
2021-11-16 17:55:16 -0300

    exibição de nome e cargo do model Cargo no painel admin

--- romenil-backend/permissoes/models.py
+++ romenil-backend/permissoes/models.py
@@ -9,4 +9,7 @@ CARGO_CHOICES = (
 class Cargo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cargo = models.CharField(max_length=50, choices=CARGO_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.cargo}'
     
