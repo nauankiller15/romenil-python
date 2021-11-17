88ca8f0a482e0f66b18ce1bde6970e308901dbda
Daniel Chaves <danielchaves001@gmail.com>
2021-11-17 17:20:59 -0300

    Nome de campo alterado no model Usuario

--- romenil-backend/conta/models.py
+++ romenil-backend/conta/models.py
@@ -11,15 +11,30 @@ from validators.usuario import cpf_or_cnpj_valid
 
 class Usuario(Model):
 
-    usuario = ForeignKey(User, on_delete=CASCADE)
+    user = ForeignKey(User, on_delete=CASCADE)
     cpf_ou_cnpj = CharField(max_length=18, unique=True, validators=[cpf_or_cnpj_valid])
     celular = CharField(max_length=15)  
 
     @property
     def ativo(self):
-        eduzz = Eduzz(self.cpf_ou_cnpj)
-        produto_ativo = eduzz.ativo
-        return produto_ativo or self.usuario.is_staff
+
+        produto_ativo = False
+
+        if self.cpf_ou_cnpj:
+            eduzz = Eduzz(self.cpf_ou_cnpj)
+            produto_ativo = eduzz.ativo
+
+        administracao = False
+        if self.user.is_staff:
+            administracao = True
+        else:
+            cargos = self.user.cargo_set.all()
+            for cargo in cargos:
+                if cargo.cargo in ['desenvolvedor', 'administrador']:
+                    administracao = True
+                    break
+
+        return produto_ativo or administracao
 
     def data_formulario(self):
         patologia = self.patologia_set.last()
@@ -27,5 +42,5 @@ class Usuario(Model):
         return patologia.modificado_em
 
     def __str__(self):
-        return f'{self.usuario.first_name} {self.usuario.last_name} - {self.cpf_ou_cnpj}'
+        return f'{self.user.first_name} {self.user.last_name} - {self.cpf_ou_cnpj}'
     
