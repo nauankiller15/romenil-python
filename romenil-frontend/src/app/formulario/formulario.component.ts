2aa8b8eb26ddd0e365a0f4c725ecea918c5f74c9
nauankiller15 <nauan.alcantara1@gmail.com>
2021-12-08 15:37:38 -0300

    ajustes visuais calculadoras'

--- romenil-frontend/src/app/formulario/formulario.component.ts
+++ romenil-frontend/src/app/formulario/formulario.component.ts
@@ -18,6 +18,15 @@ export class FormularioComponent implements OnInit {
   formulario = new Formulario();
   editavel = true;
 
+  // CARREGADOR NG SKELETON
+  animation = 'pulse';
+  contentLoadedData = false;
+  count = 10;
+  widthHeightSizeInPixels = 50;
+
+  intervalId: number | null = null;
+  //
+
   constructor(
     private apiService: ApiService,
     private accountService: AccountService,
@@ -28,13 +37,21 @@ export class FormularioComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // SKELETON LOADER
+    this.intervalId = window.setInterval(() => {
+      this.animation = this.animation === 'pulse' ? 'progress-dark' : 'pulse';
+      this.count = this.count === 4 ? 5 : 4;
+      this.widthHeightSizeInPixels =
+        this.widthHeightSizeInPixels === 50 ? 100 : 50;
+    }, 5000);
+    //---------------
+
     $(document).ready(function () {
       $('.loader-wrapper').fadeIn(250).delay(2000).fadeOut(450);
     });
     $('#fecharBtForm').on('click', function () {
       $('#formulario').fadeOut('100');
       $('body').removeClass('noborder');
-
     });
 
     // BOTÃO DE ABRIR TELA DE CONFIRMAÇÃO DE GERAR CARDÁPIO
@@ -82,6 +99,8 @@ export class FormularioComponent implements OnInit {
         if (data) {
           this.formulario = data;
 
+          this.contentLoadedData = true;
+
           let data_form = new Date(this.formulario.modificado_em);
           data_form.setDate(data_form.getDate() + 20);
           let agora = new Date();
@@ -90,6 +109,9 @@ export class FormularioComponent implements OnInit {
         }
       },
       (error) => {
+
+        this.contentLoadedData = false;
+
         const erro = new Erro(this.toastrService, error);
         erro.exibir();
       }
